db07158b79e326bc98c9c941df8f5f4f38d71875
useraid <useraid@pm.me>
2022-08-11 23:20:57 +0530

    added keepass

--- Windows/tools.bat
+++ Windows/tools.bat
@@ -1,1 +1,1 @@
-winget install --id=VideoLAN.VLC -e  && winget install --id=VMware.WorkstationPro -e  && winget install --id=RARLab.WinRAR -e  && winget install --id=Mozilla.Firefox.ESR -e  && winget install --id=Microsoft.VisualStudioCode -e 
+winget install --id=VideoLAN.VLC -e  && winget install --id=VMware.WorkstationPro -e  && winget install --id=RARLab.WinRAR -e  && winget install --id=Mozilla.Firefox.ESR -e  && winget install --id=Microsoft.VisualStudioCode -e  && winget install -e --id DominikReichl.KeePass
